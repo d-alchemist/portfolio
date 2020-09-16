57731dd2d025323e7b42d1618e3d384faa3c60ea
Chilo Nwokolo <despeakableme@gmail.com>
2020-09-16 05:47:42 +0100

    stopped tracking env local and env staging

--- src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-
-const ButtonComponent: React.FC<{ variant: string }> = (props) => {
-	return <Button variant={props.variant}>{props.children}</Button>;
-};
-
-export default ButtonComponent;
